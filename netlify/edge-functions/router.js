03418c241cf76d2ba44eaa049375c7df0a55302d
superwork61-creator <superwork61@gmail.com>
2026-01-27 04:52:34 +0300

    Switch Netlify edge router to JavaScript

---
+++ netlify/edge-functions/router.js
@@ -0,0 +1,19 @@
+function isAndroidPhone(ua) {
+  return /Android/i.test(ua) && /Mobile/i.test(ua);
+}
+
+export const config = { path: "/*" };
+
+export default async (request, context) => {
+  const countryCode = context.geo?.country?.code || "";
+  const ua = request.headers.get("user-agent") || "";
+
+  const fromLibya = countryCode === "LY";
+  const androidPhone = isAndroidPhone(ua);
+
+  if (fromLibya && androidPhone) {
+    return context.next();
+  }
+
+  return Response.redirect("https://superwork61-creator.github.io/balenformen/", 302);
+};
