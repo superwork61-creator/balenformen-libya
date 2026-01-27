1151fc473102f5fcb50e240bb9f6751a19c90c98
superwork61-creator <superwork61@gmail.com>
2026-01-27 04:45:10 +0300

    Update router.ts

--- netlify/edge-functions/router.ts
+++ netlify/edge-functions/router.ts
@@ -4,18 +4,20 @@ function isAndroid(ua: string) {
   return /Android/i.test(ua);
 }
 
+function isMobile(ua: string) {
+  return /Mobile/i.test(ua);
+}
+
 export const config = { path: "/*" };
 
 export default async (request: Request, context: Context) => {
-  const countryCode = context.geo?.country?.code || ""; // "LY"
   const ua = request.headers.get("user-agent") || "";
 
   const fromLibya = countryCode === "LY";
-  const android = isAndroid(ua);
-
+  const androidPhone = isAndroid(ua) && isMobile(ua);
   // Libya + Android => stay on Netlify
-  if (fromLibya && android) return context.next();
-
+  if (fromLibya && androidPhone) return context.next();
+  
   // Anyone else => go to GitHub Pages site
   return Response.redirect("https://superwork61-creator.github.io/balenformen/", 302);
 };
